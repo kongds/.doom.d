b3e33665680052989a06b698acefe43c139482d6
kongds <j614023177@icloud.com>
2022-11-26 12:52:26 +0800

    add popup vterm

--- configs/init-vterm.el
+++ configs/init-vterm.el
@@ -10,4 +10,92 @@
   (define-key vterm-mode-map (kbd "<insert-state> C-c") 'vterm-send-C-c)
   (define-key vterm-mode-map (kbd "<insert-state> C-b") 'vterm-send-C-b)
   (define-key vterm-mode-map (kbd "<insert-state> C-l") 'vterm--self-insert)
-  (define-key vterm-mode-map (kbd "<insert-state> C-w h") 'evil-window-left))
+  (define-key vterm-mode-map (kbd "<insert-state> C-w h") 'evil-window-left)
+
+  ;; code based on https://github.com/seagle0128/.emacs.d/blob/master/lisp/init-shell.el
+  ;; Shell Pop: leverage `popper'
+  (defvar vterm-pop--frame nil)
+  (defvar vterm-pop--window nil)
+
+  (defun vterm-pop--shell (&optional arg)
+    "Run shell and return the buffer."
+    (let* ((buffer-name
+            (format "*doom:vterm-popup:%s*"
+                    (if (bound-and-true-p persp-mode)
+                        (safe-persp-name (get-current-persp))
+                      "main")))
+           (buffer (or (cl-loop for buf in (doom-buffers-in-mode 'vterm-mode)
+                                if (equal (buffer-local-value '+vterm--id buf)
+                                          buffer-name)
+                                return buf)
+                       (get-buffer-create buffer-name))))
+      (with-current-buffer buffer
+        (setq-local +vterm--id buffer-name)
+        (unless (eq major-mode 'vterm-mode)
+          (vterm-mode)))
+      buffer))
+
+  (defun vterm-pop--hide-frame ()
+    "Hide child frame and refocus in parent frame."
+    (when (and (frame-live-p vterm-pop--frame)
+               (frame-visible-p vterm-pop--frame))
+      (make-frame-invisible vterm-pop--frame)
+      (select-frame-set-input-focus (frame-parent vterm-pop--frame))
+      (setq vterm-pop--frame nil)))
+
+  (defun vterm-pop-toggle ()
+    "Toggle shell."
+    (interactive)
+    (vterm-pop--hide-frame)
+    (if (window-live-p vterm-pop--window)
+        (progn
+          (delete-window vterm-pop--window)
+          (setq vterm-pop--window nil))
+      (setq vterm-pop--window
+            (get-buffer-window (vterm-pop--shell)))))
+
+  (defun vterm-pop-posframe-hidehandler (_)
+    "Hidehandler used by `vterm-pop-posframe-toggle'."
+    (not (eq (selected-frame) vterm-pop--frame)))
+
+  (defun vterm-pop-posframe-toggle ()
+    "Toggle shell in child frame."
+    (interactive)
+    (if (and (frame-live-p vterm-pop--frame)
+             (frame-visible-p vterm-pop--frame))
+        (progn
+          ;; Hide child frame and refocus in parent frame
+          (make-frame-invisible vterm-pop--frame)
+          (select-frame-set-input-focus (frame-parent vterm-pop--frame))
+          (setq vterm-pop--frame nil))
+      (let* ((width  (max 100 (round (* (frame-width) 0.62))))
+             (height (round (* (frame-height) 0.62)))
+             (buffer (vterm-pop--shell))
+             (window (get-buffer-window buffer)))
+        ;; Hide window: for `popper'
+        (when (window-live-p window)
+          (delete-window window))
+
+        ;; Shell pop in child frame
+        (setq vterm-pop--frame
+              (posframe-show
+               buffer
+               :poshandler #'posframe-poshandler-frame-center
+               :width width
+               :height height
+               :min-width width
+               :min-height height
+               :border-width 2
+               :border-color "gray"
+               :override-parameters '((cursor-type . t))
+               :respect-mode-line t
+               :accept-focus t))
+
+        ;; Focus in child frame
+        (select-frame-set-input-focus vterm-pop--frame)
+        (with-current-buffer buffer
+          (evil-insert 0)))))
+
+  (map! "s-f" #'vterm-pop-posframe-toggle
+        "s-;" #'vterm-pop-posframe-toggle)
+)

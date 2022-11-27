71ba0424277ff2489264c794cdebe102db698f99
kongds <j614023177@icloud.com>
2022-11-27 14:45:01 +0800

    update eaf

--- configs/init-eaf.el
+++ configs/init-eaf.el
@@ -55,17 +55,6 @@
               (lambda ()
                 (local-set-key (kbd "o") 'magit-open-repo))))
 
-  ;; override SPC
-  ;; SPC SPC to insert SPC in eaf
-  ;; (map!
-  ;;  (:leader
-  ;;  :desc "switch" :n "SPC" #'(lambda(arg)
-  ;;                              (interactive "P")
-  ;;                              (if (member (current-buffer) (eaf--get-eaf-buffers))
-  ;;                                  (eaf-call-async "eval_function" eaf--buffer-id "insert_or_scroll_up_page" "SPC")
-  ;;                                  (condition-case nil
-  ;;                                      (in-project-switch nil nil)
-  ;;                                    (error (project-find-file)))))))
 
   (add-hook 'evil-normal-state-entry-hook
             (lambda ()
@@ -80,61 +69,6 @@
   (setq eaf-proxy-host "127.0.0.1"
         eaf-proxy-port "1087"
         eaf-proxy-type "http")
-
-
-  (defun eaf--mac-switch-to-python-timer-fun ()
-    (if eaf--mac-switch-to-python
-        (let ((front (shell-command-to-string "bash /Users/royokong/fontmost_app.sh")))
-        ;;(let ((front (shell-command-to-string "app-frontmost --name")))
-          (unless (member front (list "Python\n" "python3\n" "Emacs\n"))
-            (cancel-timer eaf--mac-switch-to-python-timer)
-            (setq eaf--mac-has-focus t)
-            (setq eaf--mac-switch-to-python nil)
-            (eaf--mac-focus-out)))))
-
-
-  (defvar eaf--mac-switch-to-python-timer nil)
-
-  (defun eaf--mac-focus-change ()
-    "Manage Emacs's focus change."
-    ;; remove python timer
-    (if eaf--mac-switch-to-python-timer (cancel-timer eaf--mac-switch-to-python-timer))
-
-    (if eaf--mac-safe-focus-change
-        (if (executable-find "app-frontmost")
-            (let ((front (shell-command-to-string "bash /Users/royokong/fontmost_app.sh")))
-            ;;(let ((front (shell-command-to-string "app-frontmost --name")))
-              (cond
-               ((member front (list "Python\n" "python3\n"))
-                (setq eaf--mac-switch-to-python t)
-                (setq eaf--mac-switch-to-python-timer (run-with-timer 0.3 1
-                                                                      'eaf--mac-switch-to-python-timer-fun)))
-               ((string= "Emacs\n" front)
-                (cond
-                 (eaf--mac-switch-to-python
-                  (setq eaf--mac-switch-to-python nil))
-                 ((not eaf--mac-has-focus)
-                  (run-with-timer 0.1 nil #'eaf--mac-focus-in))
-                 (eaf--mac-has-focus
-                  (eaf--mac-focus-out))))
-               (t (eaf--mac-focus-out))))
-          (message "Please install app-frontmost from https://pypi.org/project/mac-app-frontmost/ to make EAF works with macOS platform."))
-      (setq eaf--mac-unsafe-focus-change-timer
-            (unless eaf--mac-unsafe-focus-change-timer
-              (run-at-time 0.06 nil
-                              #'eaf--mac-unsafe-focus-change-handler)))))
-
-  (defun eaf--mac-replace-eaf-buffers ()
-    (let ((current-window (selected-window))
-          (need-select-current-window nil))
-      (dolist (window (window-list))
-        (when (eq (with-current-buffer (window-buffer window)
-                    major-mode)  'eaf-mode)
-          (setq need-select-current-window t)
-          (select-window window)
-          (get-buffer-create "*eaf temp*")
-          (switch-to-buffer "*eaf temp*" t)))
-      (if need-select-current-window (select-window current-window))))
 
   ;; ensure focus change function has been add
   (advice-add 'eaf-restart-process :after

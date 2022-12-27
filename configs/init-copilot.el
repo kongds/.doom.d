c25946d8ebdc79861f618cf6bfd2609cecf98b68
kongds <j614023177@icloud.com>
2022-12-27 15:56:50 +0800

    update diff

--- configs/init-copilot.el
+++ configs/init-copilot.el
@@ -4,7 +4,7 @@
   :hook (prog-mode . copilot-mode)
   :custom
   (copilot-network-proxy '(:host "127.0.0.1" :port 1087))
-  (copilot-idle-delay 0)
+  (copilot-idle-delay 0.2)
   :config
   (define-key evil-insert-state-map (kbd "C-e") 'copilot-accept-completion)
   (define-key evil-insert-state-map (kbd "M-]") 'copilot-next-completion)
